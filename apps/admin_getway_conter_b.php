15dab23f769f23985c0bc467d58d1ac517de996e
leomastakusuma <leomastakusumaa@gmail.com>
2018-04-05 14:05:13 +0700

    add fiel

---
+++ apps/admin_getway_conter_b.php
@@ -0,0 +1,20 @@
+<?php
+	include "mysql_connect.php";
+	$current = !empty($_POST) ? $_POST : '';
+	$rstClient = $mysqli->query('SELECT count(*) as count FROM data_antrian where type="B"');
+	$rowClient = $rstClient->fetch_array();
+	$jmlClient = 1;
+
+	if($rowClient['count']>0){
+		$jmlClient = $rowClient['count'] + 1;
+	}
+
+
+	if(!empty($_POST)){
+		$results = $mysqli->query("INSERT INTO data_antrian (id,waktu,counter,status,type) VALUES (".$jmlClient.",'','0',3,'B')");
+		$jmlClient = $jmlClient+1;
+
+	}
+	echo json_encode( array('next'=> $jmlClient) );
+	include 'mysql_close.php';
+    
